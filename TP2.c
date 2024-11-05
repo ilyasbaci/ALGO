71049040392ff2811aac84f928c5fdfc51cd2428
Ilyas Baci <ilyasbaci@gmail.com>
2024-11-05 09:24:44 +0100

    Update TP2.c

---
+++ TP2.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char* ChargerChaine(int N);
+int Longueur(char* ch);
+void InverserTab(char Tab[], char T[], int m);
+void ChargerTab(char* p, char Tab[]);
+void AfficherTab(char Tab[], int m);
+
+int main() {
+    char* ch;
+    int n;
+
+    printf("Veuillez saisir la taille maximale de la chaine :\n");
+    scanf("%d", &n);
+
+    ch = ChargerChaine(n);
+    int m = Longueur(ch);
+
+    char Tab[m], T[m];
+    ChargerTab(ch, Tab);
+    AfficherTab(Tab, m);
+    InverserTab(Tab, T, m);
+    AfficherTab(T, m);
+
+    free(ch);
+    return 0;
+}
+
+char* ChargerChaine(int N) {
+    char* chaine = (char*)malloc((N + 1) * sizeof(char));
+    if (chaine == NULL) {
+        printf("Erreur d'allocation de memoire.\n");
+        exit(1);
+    }
+    printf("Entrez la chaine: ");
+    scanf("%s", chaine);
+    return chaine;
+}
+
+int Longueur(char* ch) {
+    return strlen(ch);
+}
+
+void InverserTab(char Tab[], char T[], int m) {
+    for (int i = 0; i < m; i++) {
+        T[i] = Tab[m - i - 1];
+    }
+    T[m] = '\0';
+}
+
+void ChargerTab(char* p, char Tab[]) {
+    strcpy(Tab, p);
+}
+
+void AfficherTab(char Tab[], int m) {
+    printf("%s\n", Tab);
+}
