3fd9120dedf909e55db257dc6639002234225ad0
Ilyas Baci <ilyasbaci@gmail.com>
2024-11-19 09:16:30 +0100

    Update TP1ex2.c

--- TP1ex2.c
+++ TP1ex2.c
@@ -3,7 +3,6 @@
 int main() {
     int T[10][10];
     int i , j , save , size;
-    //read inputs from user :
     printf("enter size of array : ");
     scanf("%d",&size);
     for(i = 0 ; i < size ; i++){
@@ -12,7 +11,6 @@ int main() {
          scanf("%d",&T[i][j]);
         }
     }
-    //print original array :
     printf("original array :\n\n");
     for(i = 0 ; i < size ; i++){
         for(j = 0 ; j < size ; j++){
@@ -20,7 +18,6 @@ int main() {
         }
         printf("\n\n");
     }
-    //reverse the array :
     for(i = 1 ; i < size ; i++){
         for(j = 0 ; j < i ; j++){
          save = T[i][j];
@@ -28,7 +25,6 @@ int main() {
          T[j][i] = save;
         }
     }
-    //print reversed array :
     printf("reversed array :\n\n");
     for(i = 0 ; i < size ; i++){
         for(j = 0 ; j < size ; j++){
